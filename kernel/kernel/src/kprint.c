7688181520bf60b3dfc43be3e7e4645d6f08e70b
quang1610 <nguyenducquang1610@gmail.com>
2022-02-03 01:12:25 -0600

    Add printf

--- kernel/kernel/src/kprint.c
+++ kernel/kernel/src/kprint.c
@@ -53,43 +53,48 @@ void kprint_d(uint64_t value) {
 // Print an unsigned 64-bit integer value to the terminal in lowercase
 // hexadecimal notation (no leading zeros or “0x” please!)
 void kprint_x(uint64_t value) {
-	if (value == 0) {
-		kprint_c('0');
-	} else {
-		uint64_t remain = 0;
-		size_t num_digit = 0;
+  if (value == 0) {
+    kprint_c('0');
+  } else {
+    uint64_t remain = 0;
+    size_t num_digit = 0;
 
-		// using the global buffer to store the stringified hex value
-		buffer_hex_uint64[NUM_DIGIT_HEX_UINT64] = '\0';
-		char* cursor = buffer_hex_uint64 + NUM_DIGIT_HEX_UINT64;
+    // using the global buffer to store the stringified hex value
+    buffer_hex_uint64[NUM_DIGIT_HEX_UINT64] = '\0';
+    char* cursor = buffer_hex_uint64 + NUM_DIGIT_HEX_UINT64;
 
-		// Fill in the buffer from the end to the beginning with each digit of value
+    // Fill in the buffer from the end to the beginning with each digit of value
     // (from right to left)
     while (value != 0) {
       remain = value % 16;
       value = value / 16;
 
-			// Decrease cursor by 1, set character based on the hex form of remain
+      // Decrease cursor by 1, set character based on the hex form of remain
       *(--cursor) = remain < 10 ? '0' + remain : 'a' - 10 + remain;
       num_digit++;
     }
 
     term_write(cursor, num_digit);
-	}
+  }
 }
 
 // Print the value of a pointer to the terminal in lowercase hexadecimal with
 // the prefix “0x”
 void kprint_p(void* ptr) {
-	kprint_s("0x");
-	kprint_x((uint64_t) ptr);
+  kprint_s("0x");
+  kprint_x((uint64_t)ptr);
 }
 
+// Print formatted string, supporting:
+// - %s for string
+// - %c for character
+// - %d for decimal number
+// - %x for hex number
+// - %p for pointer address
 void kprintf(const char* format, ...) {
   const char* cursor = format;
-
-  // Set up va_list to read arguments 
-  va_list args; 
+  // Set up va_list to read arguments
+  va_list args;
   va_start(args, format);
 
   // Read each character in format and handle each case when we see %
@@ -98,33 +103,33 @@ void kprintf(const char* format, ...) {
       // Process the character right after the '%'
       cursor++;
       switch (*cursor) {
-        // case "%" -> print nothing
-        case '\0':
-          return;
-        // case "%%" --> print '%' 
+        // case "%%" --> print '%'
         case '%':
           kprint_c('%');
           break;
-        // case "%c" --> print next arg as a character 
+        // case "%c" --> print next arg as a character
         case 'c':
           kprint_c(va_arg(args, int));
           break;
-        // case "%s" --> print next arg as a string 
+        // case "%s" --> print next arg as a string
         case 's':
           kprint_s(va_arg(args, char*));
           break;
-        // case "%d" --> print next arg as a decimal number 
+        // case "%d" --> print next arg as a decimal number
         case 'd':
           kprint_d(va_arg(args, uint64_t));
           break;
-        // case "%x" --> print next arg as a hex number 
+        // case "%x" --> print next arg as a hex number
         case 'x':
           kprint_x(va_arg(args, uint64_t));
           break;
-        // case "%p" --> print next arg as a pointer address 
+        // case "%p" --> print next arg as a pointer address
         case 'p':
           kprint_x(va_arg(args, void*));
           break;
+        // case "%" -> print nothing, return
+        case '\0':
+          return;
         // unsupported escape character
         default:
           kprint_s("<not supported>");
@@ -134,5 +139,4 @@ void kprintf(const char* format, ...) {
     }
     cursor++;
   }
-
 }
